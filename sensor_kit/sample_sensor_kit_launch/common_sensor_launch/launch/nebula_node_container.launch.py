dbd78cf02c727aab6a6291b6bd47ac94bf1f67ff
Taekjin LEE <taekjin.lee@tier4.jp>
2026-01-19 14:06:58 +0900

    fix launcher for latest nebula repo

--- sensor_kit/sample_sensor_kit_launch/common_sensor_launch/launch/nebula_node_container.launch.py
+++ sensor_kit/sample_sensor_kit_launch/common_sensor_launch/launch/nebula_node_container.launch.py
@@ -73,14 +73,13 @@ def launch_setup(context, *args, **kwargs):
     # Model and make
     sensor_model = LaunchConfiguration("sensor_model").perform(context)
     sensor_make, sensor_extension = get_lidar_make(sensor_model)
-    nebula_decoders_share_dir = get_package_share_directory("nebula_decoders")
+    nebula_decoders_share_dir = get_package_share_directory("nebula_" + sensor_make.lower() + "_decoders")
 
     # Calibration file
     if sensor_extension is not None:  # Velodyne and Hesai
         sensor_calib_fp = os.path.join(
             nebula_decoders_share_dir,
             "calibration",
-            sensor_make.lower(),
             sensor_model + sensor_extension,
         )
         assert os.path.exists(
